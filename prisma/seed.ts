2b4c073668d19f0b912346396619bb40071b5f3c
Yoshitaro Takaesu <useakat@gmail.com>
2024-12-10 14:15:52 +0000

    fix: ダミーデータの購入先を「生協」「店舗」「ネット」の3つに修正し、データベースを更新しました

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,87 +2,76 @@ import { PrismaClient, Status } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-const stores = [
-  '西友',
-  'イオン',
-  'コストコ',
-  'カルディ',
-  '業務スーパー',
-  'セブンイレブン',
-  'ファミリーマート',
-  'ローソン',
-  'ドン・キホーテ',
-  'マツモトキヨシ'
-]
+const stores = ['生協', '店舗', 'ネット']
 
 const products = [
   {
     name: '食パン',
     description: '毎日の朝食用の食パン',
-    purchase_location: stores[0],
+    purchase_location: stores[0], // 生協
     stock_quantity: 2,
     minimum_stock: 3
   },
   {
     name: 'トイレットペーパー',
     description: '12ロール入り パック',
-    purchase_location: stores[1],
+    purchase_location: stores[1], // 店舗
     stock_quantity: 3,
     minimum_stock: 2
   },
   {
     name: '牛乳',
     description: '1000ml 成分無調整',
-    purchase_location: stores[2],
+    purchase_location: stores[0], // 生協
     stock_quantity: 1,
     minimum_stock: 2
   },
   {
     name: 'コーヒー豆',
     description: 'ブラジル産 中煎り',
-    purchase_location: stores[3],
+    purchase_location: stores[2], // ネット
     stock_quantity: 2,
     minimum_stock: 1
   },
   {
     name: '米',
     description: '新潟県産コシヒカリ 5kg',
-    purchase_location: stores[4],
+    purchase_location: stores[1], // 店舗
     stock_quantity: 1,
     minimum_stock: 2
   },
   {
     name: 'ティッシュペーパー',
     description: '5箱パック',
-    purchase_location: stores[5],
+    purchase_location: stores[1], // 店舗
     stock_quantity: 2,
     minimum_stock: 3
   },
   {
     name: 'シャンプー',
     description: '詰め替え用 400ml',
-    purchase_location: stores[9],
+    purchase_location: stores[2], // ネット
     stock_quantity: 1,
     minimum_stock: 1
   },
   {
     name: 'ハンドソープ',
     description: '泡タイプ 詰め替え用',
-    purchase_location: stores[9],
+    purchase_location: stores[2], // ネット
     stock_quantity: 1,
     minimum_stock: 2
   },
   {
     name: 'キッチンペーパー',
     description: '3ロール入り',
-    purchase_location: stores[1],
+    purchase_location: stores[1], // 店舗
     stock_quantity: 2,
     minimum_stock: 1
   },
   {
     name: '歯磨き粉',
     description: 'ミント味 大容量',
-    purchase_location: stores[9],
+    purchase_location: stores[0], // 生協
     stock_quantity: 1,
     minimum_stock: 2
   }
